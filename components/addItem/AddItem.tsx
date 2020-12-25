2c8c76498d31236811027f3b4c161a88247755af
Donald Wu <yeukfei02@gmail.com>
2020-12-25 19:21:05 +0800

    style: 💄 update back arrow color

--- components/addItem/AddItem.tsx
+++ components/addItem/AddItem.tsx
@@ -311,8 +311,8 @@ function UserLoggedInView(): JSX.Element {
     <div className="m-10">
       <div className="flex flex-row justify-between mb-8">
         <div className="h-6 w-6 self-center" style={{ cursor: 'pointer' }} onClick={() => handleBackArrowClick()}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
         </div>
 
