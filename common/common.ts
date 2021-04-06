59e297a4e7b808afbc6e35674b1488b274b811c3
Donald Wu <yeukfei02@gmail.com>
2021-04-06 13:42:07 +0800

    fix: 🐛 fix rooturl

--- common/common.ts
+++ common/common.ts
@@ -1,4 +1,4 @@
 export const getRootUrl = (): string => {
-  const ROOT_URL = `https://qtwaxcn004.execute-api.ap-southeast-1.amazonaws.com/prod/`;
+  const ROOT_URL = `https://p2e5httozl.execute-api.ap-southeast-1.amazonaws.com/prod/`;
   return ROOT_URL;
 };
